4ddeec181b8848a8a775de2f8c4d372a41763616
ErsaGun Tosun <ersagunfatihdev@gmail.com>
2024-04-27 11:48:43 -0700

    UPDATE todo bar add inputs and buttons to todo bar

--- src/components/ToDoBar/ToDoBar.js
+++ src/components/ToDoBar/ToDoBar.js
@@ -2,8 +2,40 @@ import React from 'react'
 
 function ToDoBar() {
   return (
-    <div className='w-5/6 h-full bg-red-500 '> 
-      ToDoBar
+    <div className='w-5/6 h-full flex flex-wrap items-center bg-red-500'>
+
+      {/* Add Task Input */}
+      <div className='basis-3/5'>
+        <div class="relative">
+          <input type="search" id="default-search" class="block w-full p-3.5 border-1 border text-sm outline-0" placeholder="Task Title" required />
+          <button type="submit" class="text-white absolute end-2.5 bottom-2.5 font-medium rounded-lg text-sm px-4 py-2 bg-black ">Add</button>
+        </div>
+      </div>
+
+
+      {/* Priority Dropdown */}
+      <div className='basis-1/5'>
+        <select id="priority" class="bg-gray-50 m-0 text-gray-900 text-sm  block w-full p-4 outline-none">
+          <option selected>Choose a priority</option>
+          <option value="5">High Priority</option>
+          <option value="4">Medium Priority</option>
+          <option value="2">Low Priority</option>
+          <option value="1">No Priority</option>
+        </select>
+      </div>
+
+      {/* Date Picker */}
+      <div className='basis-1/5'>
+        <button type="button" class="text-white w-full bg-gray-900 font-medium border-none  text-sm px-5 py-4 me-2 "> Task Last Time</button>
+      </div>
+
+      {/* Description Input */}
+      <div className='basis-1 flex-1 mt-1'>
+        <div>
+          <input type="text" id="first_name" class="bg-gray-50 border border-gray-300 text-gray-900 text-sm   block w-full p-3" placeholder="Task Description" required />
+        </div>
+      </div>
+
     </div>
   )
 }
--- src/components/ToDoBody/ToDoBody.js
+++ src/components/ToDoBody/ToDoBody.js
@@ -5,7 +5,7 @@ import ToDoBar from '../ToDoBar/ToDoBar'
 function ToDoBody() {
   return (
     <div class="h-full w-full flex flex-col bg-yellow-500 ">
-      <div class="h-20 flex justify-center bg-blue-500">
+      <div class="h-28 flex justify-center bg-blue-500">
         <ToDoBar />
       </div>
       <div class="w-full h-full flex-1 mx-auto p-4 text-lg bg-red-500 shadow-lg ">
